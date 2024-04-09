30007fdcd15866da2501b8da4bc8ae6157990046
Werner Schröter <werner.schroeter@geosphere.de>
2024-04-09 19:02:01 +0200

    integrate pre-commit check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,3 +67,15 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
+
+tasks.register<Copy>("copyPreCommitHook") {
+    description = "Copy pre-commit git hook from the scripts to the .git/hooks folder."
+    group = "git hooks"
+    outputs.upToDateWhen { false }
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+
+tasks.build {
+    dependsOn("copyPreCommitHook")
+}
